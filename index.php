756a16c97bb20e48c5c3105115eaaa9a4dd2b663
John Hurst <johnhurst@gmail.com>
2022-10-08 12:41:29 -0400

    first commit - add index.php - add action

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <?php echo '<p>Hello World</p>'; 
+        for($i=0; $i<=5;$i++) {
+            echo '<br>';
+            echo 'Value of i is '. $i;
+        }
+    ?> 
+</body>
+</html>
